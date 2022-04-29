f721999bcf8baa20a19aa8699f140dc755c5b12d
LazyDuchess <42678262+LazyDuchess@users.noreply.github.com>
2022-04-29 09:12:50 -0300

    Update Licenses

--- Assets/Scripts/Files/Formats/AbstractCodec.cs
+++ Assets/Scripts/Files/Formats/AbstractCodec.cs
@@ -1,4 +1,10 @@
-﻿using OpenTS2.Common;
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/. 
+ */
+
+using OpenTS2.Common;
 using OpenTS2.Content;
 using System;
 using System.Collections.Generic;
