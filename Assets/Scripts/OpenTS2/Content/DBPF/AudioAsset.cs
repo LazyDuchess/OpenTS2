7af13d86e4f1b41d6ad306a08e13eb132e011372
LazyDuchess <42678262+LazyDuchess@users.noreply.github.com>
2022-12-20 21:05:05 -0300

    De-partialize AudioAsset.

--- Assets/Scripts/OpenTS2/Content/DBPF/AudioAsset.cs
+++ Assets/Scripts/OpenTS2/Content/DBPF/AudioAsset.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using OpenTS2.Engine.Audio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace OpenTS2.Content.DBPF
 {
-    public partial class AudioAsset : AbstractAsset
+    public class AudioAsset : AbstractAsset
     {
         public byte[] AudioData;
+        public AudioClip Clip
+        {
+            get
+            {
+                if (_clip == null)
+                    _clip = WavUtility.ToAudioClip(AudioData, 0, TGI.ToString());
+                return _clip;
+            }
+        }
+        AudioClip _clip;
     }
 }
