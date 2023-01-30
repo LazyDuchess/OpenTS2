d91572c204145d104eb309529eb96876aae33aeb
LazyDuchess <42678262+LazyDuchess@users.noreply.github.com>
2023-01-30 17:15:14 -0300

    Added OpenTS2/Resource Key/From Filename menu item to Editor, to allow browsing for Instance IDs

---
+++ Assets/Editor/IDGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenTS2.Files.Formats.DBPF;
+using UnityEditor;
+using OpenTS2.Common;
+
+namespace OpenTS2.Editor
+{
+    class IDGenerator : EditorWindow
+    {
+        bool _hasResult = false;
+        ResourceKey _result = ResourceKey.Default;
+        string _filename;
+        void OnGUI()
+        {
+            _filename = EditorGUILayout.TextField("Resource filename: ", _filename);
+
+            if (GUILayout.Button("OK"))
+            {
+                _hasResult = true;
+                _result = new ResourceKey(_filename, 0, 0);
+            }
+
+            if (GUILayout.Button("Cancel"))
+                Close();
+
+            if (_hasResult)
+            {
+                EditorGUILayout.LabelField(_filename+":");
+                EditorGUILayout.LabelField("Instance: 0x" + _result.InstanceID.ToString("X8"));
+                EditorGUILayout.LabelField("Instance(High): 0x" + _result.InstanceHigh.ToString("X8"));
+            }
+        }
+        [MenuItem("OpenTS2/Resource Key/From Filename")]
+        private static void GenerateInstanceID()
+        {
+            IDGenerator window = new IDGenerator();
+            window.ShowUtility();
+        }
+    }
+}
