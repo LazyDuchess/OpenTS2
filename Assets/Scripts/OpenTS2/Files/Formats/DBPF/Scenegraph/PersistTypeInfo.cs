7f3072ce19feba516c980c372a41d50859ce4a21
Ammar Askar <ammar@ammaraskar.com>
2023-02-02 19:00:42 -0500

    Implement Scenegraph parsing

---
+++ Assets/Scripts/OpenTS2/Files/Formats/DBPF/Scenegraph/PersistTypeInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using OpenTS2.Files.Utils;
+
+namespace OpenTS2.Files.Formats.DBPF.Scenegraph
+{
+    /// <summary>
+    /// A common prefix used in many areas of Scenegraph serialization/deserialization. 
+    /// </summary>
+    public readonly struct PersistTypeInfo
+    {
+        public string Name { get; }
+        public uint TypeId { get; }
+        public uint Version { get; }
+
+        public PersistTypeInfo(string name, uint typeId, uint version) =>
+            (Name, TypeId, Version) = (name, typeId, version);
+
+        public static PersistTypeInfo Deserialize(IoBuffer reader)
+        {
+            var name = reader.ReadVariableLengthPascalString();
+            var typeId = reader.ReadUInt32();
+            var version = reader.ReadUInt32();
+            return new PersistTypeInfo(name, typeId, version);
+        }
+
+        public void Serialize(BinaryWriter writer)
+        {
+            writer.Write(Name);
+            writer.Write(TypeId);
+            writer.Write(Version);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(TypeId)}: {TypeId:X}, {nameof(Version)}: {Version}, {nameof(Name)}: {Name}";
+        }
+    }
+}
---
+++ Assets/Tests/OpenTS2/Files/Formats/DBPF/Scenegraph/PeristTypeInfoTest.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using NUnit.Framework;
+using OpenTS2.Files.Formats.DBPF.Scenegraph;
+using OpenTS2.Files.Utils;
+
+public class PeristTypeInfoTest
+{
+    [Test]
+    public void DeserializesFromKnownData()
+    {
+        var cImageDataTypeInfo = new byte[]
+        {
+            0x0A, 0x63, 0x49, 0x6D, 0x61, 0x67, 0x65, 0x44, 0x61, 0x74, 0x61, 0x6C, 0x27, 0x4A, 0x1C, 0x09, 0x00, 0x00,
+            0x00
+        };
+
+        var reader = new IoBuffer(new MemoryStream(cImageDataTypeInfo));
+        reader.ByteOrder = ByteOrder.LITTLE_ENDIAN;
+
+        var typeInfo = PersistTypeInfo.Deserialize(reader);
+        Assert.That(typeInfo.Name, Is.EqualTo("cImageData"));
+        Assert.That(typeInfo.TypeId, Is.EqualTo(0x1C4A276C));
+        Assert.That(typeInfo.Version, Is.EqualTo(9));
+    }
+
+    [Test]
+    public void TypeInfoRoundTripsSuccessfully()
+    {
+        var typeInfo = new PersistTypeInfo("cFakeNode", 0xbeef, 1);
+
+        var stream = new MemoryStream();
+        var writer = new BinaryWriter(stream);
+        typeInfo.Serialize(writer);
+
+        var reader = new IoBuffer(stream);
+        reader.ByteOrder = ByteOrder.LITTLE_ENDIAN;
+        reader.Seek(SeekOrigin.Begin, 0);
+
+        var deserializedTypeInfo = PersistTypeInfo.Deserialize(reader);
+        Assert.That(deserializedTypeInfo, Is.EqualTo(typeInfo));
+    }
+}
