de746da2e4b8f9541fb09087b6d4bc8166071c96
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-05-30 09:49:37 -0300

    Add sledgehammer cursor special case

--- Assets/Scripts/OpenTS2/UI/CursorController.cs
+++ Assets/Scripts/OpenTS2/UI/CursorController.cs
@@ -13,10 +13,17 @@ namespace OpenTS2.UI {
         public enum CursorType
         {
             Default,
-            Hourglass
+            Hourglass,
+            Sledgehammer
         }
 
         public static CursorType Cursor = CursorType.Default;
+
+        private void RegisterCursorFromLatestProduct(CursorType type, string filename)
+        {
+            var epManager = EPManager.Get();
+            RegisterCursor(type, epManager.GetLatestProduct(), filename);
+        }
 
         private void RegisterCursor(CursorType type, ProductFlags product, string filename)
         {
@@ -43,6 +50,8 @@ namespace OpenTS2.UI {
         {
             RegisterCursor(CursorType.Default, ProductFlags.BaseGame, "arrow_8.cur");
             RegisterCursor(CursorType.Hourglass, ProductFlags.BaseGame, "Hourglass_8.ani");
+            // Sledgehammer was added in Pets, IIRC, so Pets and later all come with the cursor in the UI files.
+            RegisterCursorFromLatestProduct(CursorType.Sledgehammer, "Sledgehammer_8.cur");
         }
 
 #if !UNITY_EDITOR
