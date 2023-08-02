d31cda97ad4cfcdab55b399abedf955b6ee238e4
Ammar Askar <ammar@ammaraskar.com>
2023-08-02 13:24:33 -0400

    Add texture to particle effects

--- Assets/Scripts/OpenTS2/Scenes/EffectsManager.cs
+++ Assets/Scripts/OpenTS2/Scenes/EffectsManager.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using OpenTS2.Common;
 using OpenTS2.Content;
 using OpenTS2.Content.DBPF;
 using OpenTS2.Content.DBPF.Effects;
+using OpenTS2.Content.DBPF.Scenegraph;
+using OpenTS2.Engine;
 using OpenTS2.Files;
 using OpenTS2.Files.Formats.DBPF;
 using UnityEngine;
@@ -14,6 +17,7 @@ namespace OpenTS2.Scenes
     public class EffectsManager : MonoBehaviour
     {
         private EffectsAsset _effects;
+        private List<Material> _particleMaterials = new List<Material>();
 
         private void Start()
         {
@@ -26,6 +30,15 @@ namespace OpenTS2.Scenes
             // Disable emission on the base particle system, only children will emit.
             var emissionModule = GetComponent<ParticleSystem>().emission;
             emissionModule.enabled = false;
+        }
+
+        // Clean up the materials we created.
+        private void OnDestroy()
+        {
+            foreach(var mat in _particleMaterials)
+            {
+                mat.Free();
+            }
         }
 
         public void StartEffect(string effectName)
@@ -50,14 +63,35 @@ namespace OpenTS2.Scenes
             unityParticleSystem.Play(withChildren:true);
         }
 
-        private static GameObject CreateForParticleEffect(EffectDescription description, ParticleEffect effect)
+        private GameObject CreateForParticleEffect(EffectDescription description, ParticleEffect effect)
         {
             var particleObject = new GameObject(description.EffectName, typeof(ParticleSystem));
             var system = particleObject.GetComponent<ParticleSystem>();
+            system.Stop(withChildren:true, ParticleSystemStopBehavior.StopEmittingAndClear);
 
             var emission = system.emission;
             var emissionRateOverTime = emission.rateOverTime;
             emissionRateOverTime.curve = effect.Emission.RateCurve.ToUnityCurve();
+
+            var main = system.main;
+            main.duration = effect.Life.Life[0];
+
+            if (effect.Drawing.MaterialName != "")
+            {
+                var textureAsset = ContentProvider.Get().GetAsset<ScenegraphTextureAsset>(new ResourceKey(
+                    $"{effect.Drawing.MaterialName}_txtr", GroupIDs.Scenegraph, TypeIDs.SCENEGRAPH_TXTR));
+
+                // TODO: temporarily using the standard shader here.
+                var material = new Material(Shader.Find("OpenTS2/StandardMaterial/AlphaBlended"))
+                {
+                    mainTexture = textureAsset.GetSelectedImageAsUnityTexture(ContentProvider.Get())
+                };
+                _particleMaterials.Add(material);
+
+                system.GetComponent<Renderer>().material = material;
+
+                Debug.Log($"material: {effect.Drawing.MaterialName}");
+            }
 
             return particleObject;
         }
