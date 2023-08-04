7971682266b7bad9b0f6ca59b54081bb6b5cd428
Ammar Askar <ammar@ammaraskar.com>
2023-08-04 11:47:46 -0400

    Refactor particles system to make meta-particles easier

--- Assets/Scripts/OpenTS2/Scenes/ParticleEffects/SwarmParticleSystem.cs
+++ Assets/Scripts/OpenTS2/Scenes/ParticleEffects/SwarmParticleSystem.cs
@@ -15,7 +15,6 @@ namespace OpenTS2.Scenes.ParticleEffects
     [RequireComponent(typeof(ParticleSystem))]
     public class SwarmParticleSystem : MonoBehaviour
     {
-
         public void SetVisualEffect(SwarmVisualEffect visualEffect, EffectsAsset effectsAsset)
         {
             // Disable the particle system that belongs to this component. We only use it to play all the child particle
@@ -33,7 +32,8 @@ namespace OpenTS2.Scenes.ParticleEffects
                         particleEffectSystem.transform.SetParent(transform, worldPositionStays: false);
                         break;
                     case MetaParticle e:
-                        var metaParticleSystem = CreateGameObjectForMetaParticleSystem(effectDescription.EffectName, e, effectsAsset);
+                        var metaParticleSystem =
+                            CreateGameObjectForMetaParticleSystem(effectDescription.EffectName, e, effectsAsset);
                         metaParticleSystem.transform.SetParent(transform, worldPositionStays: false);
                         break;
                     case ModelEffect e:
@@ -58,7 +58,8 @@ namespace OpenTS2.Scenes.ParticleEffects
             GetComponent<ParticleSystem>().Play(withChildren: true);
         }
 
-        private static GameObject CreateGameObjectForMetaParticleSystem(string effectName, MetaParticle e, EffectsAsset effectsAsset)
+        private static GameObject CreateGameObjectForMetaParticleSystem(string effectName, MetaParticle e,
+            EffectsAsset effectsAsset)
         {
             // A meta-particle is basically "extra-stuff" like an altered lifetime or emission rate attached
             // to an existing particle effect. For now we just handle some particular types of child effects.
@@ -71,7 +72,8 @@ namespace OpenTS2.Scenes.ParticleEffects
                     effectsAsset.GetEffectFromVisualEffectDescription(desc);
                 if (!(baseEffect is ModelEffect modelEffect))
                 {
-                    throw new NotImplementedException($"Can't handle meta-particles with a base effect of {baseEffect}");
+                    throw new NotImplementedException(
+                        $"Can't handle meta-particles with a base effect of {baseEffect}");
                 }
 
                 var particleObject = new GameObject(desc.EffectName, typeof(SwarmMetaParticleSystem));
@@ -83,60 +85,19 @@ namespace OpenTS2.Scenes.ParticleEffects
             return metaParticles;
         }
 
-        private static GameObject CreateChildSystemForParticleEffect(EffectDescription description, ParticleEffect effect)
+        private static GameObject CreateChildSystemForParticleEffect(EffectDescription description,
+            ParticleEffect effect)
         {
-            var particleObject = new GameObject(description.EffectName, typeof(ParticleSystem), typeof(AssetReferenceComponent));
+            var particleObject = new GameObject(description.EffectName, typeof(ParticleSystem),
+                typeof(AssetReferenceComponent));
             var system = particleObject.GetComponent<ParticleSystem>();
             system.Stop(withChildren: true, ParticleSystemStopBehavior.StopEmittingAndClear);
 
-            var emission = system.emission;
-            emission.rateOverTime = effect.Emission.RateCurve.ToUnityCurve();
-
-            // Set the emitter shape and direction.
-            var shape = system.shape;
-            var (emitterPos, emitterRotation, emitterScale) = effect.Emission.EmitVolume.GetCenterRotationAndScale();
-            shape.position = emitterPos;
-            shape.rotation = emitterRotation.eulerAngles;
-            shape.scale = emitterScale;
-            if (effect.Emission.EmitTorusWidth > 0)
-            {
-                shape.shapeType = ParticleSystemShapeType.Donut;
-            }
-            else if (effect.IsFlagSet(ParticleFlagBits.EmitterIsEllipsoid))
-            {
-                shape.shapeType = ParticleSystemShapeType.Sphere;
-            }
-            else
-            {
-                shape.shapeType = ParticleSystemShapeType.Box;
-            }
-
-            // Set particle lifetime and speed.
-            var main = system.main;
-            main.startLifetime = new ParticleSystem.MinMaxCurve(min: effect.Life.Life[0], max: effect.Life.Life[1]);
-            main.startSpeed =
-                new ParticleSystem.MinMaxCurve(min: effect.Emission.EmitSpeed[0], max: effect.Emission.EmitSpeed[1]);
-
-            // Particle direction.
-            var velocityOverTime = system.velocityOverLifetime;
-            velocityOverTime.enabled = true;
-            velocityOverTime.x = new ParticleSystem.MinMaxCurve(effect.Emission.EmitDirection.LowerCorner[0],
-                effect.Emission.EmitDirection.UpperCorner[0]);
-            velocityOverTime.y = new ParticleSystem.MinMaxCurve(effect.Emission.EmitDirection.LowerCorner[1],
-                effect.Emission.EmitDirection.UpperCorner[1]);
-            velocityOverTime.z = new ParticleSystem.MinMaxCurve(effect.Emission.EmitDirection.LowerCorner[2],
-                effect.Emission.EmitDirection.UpperCorner[2]);
-
-            // Set particle size over time.
-            var sizeOverTime = system.sizeOverLifetime;
-            sizeOverTime.size = effect.Size.SizeCurve.ToUnityCurveWithVariance(effect.Size.SizeVary);
-            sizeOverTime.enabled = true;
-
-            // Set colors.
-            var colorOverLifetime = system.colorOverLifetime;
-            var (minColorGradient, maxColorGradient) = effect.Color.GetColorGradientsOverTime();
-            colorOverLifetime.color = new ParticleSystem.MinMaxGradient(minColorGradient, maxColorGradient);
-            colorOverLifetime.enabled = true;
+            SetParticleEmitterRateAndShape(system.emission, system.shape, effect, effect.Emission);
+            SetParticleSpeedAndLifetime(system.main, effect);
+            SetParticleDirection(system.velocityOverLifetime, effect.Emission);
+            SetParticleSizeOverTime(system.sizeOverLifetime, effect.Size);
+            SetParticleColorOverTime(system.colorOverLifetime, effect.Color);
 
             if (effect.Drawing.MaterialName != "")
             {
@@ -150,6 +111,64 @@ namespace OpenTS2.Scenes.ParticleEffects
             }
 
             return particleObject;
+        }
+
+        private static void SetParticleEmitterRateAndShape(ParticleSystem.EmissionModule emissionModule,
+            ParticleSystem.ShapeModule shape, ParticleEffect effect, ParticleEmission emission)
+        {
+            // Set emitter rate.
+            emissionModule.rateOverTime = emission.RateCurve.ToUnityCurve();
+
+            // Set the emitter shape.
+            var (emitterPos, emitterRotation, emitterScale) = emission.EmitVolume.GetCenterRotationAndScale();
+            shape.position = emitterPos;
+            shape.rotation = emitterRotation.eulerAngles;
+            shape.scale = emitterScale;
+            if (emission.EmitTorusWidth > 0)
+            {
+                shape.shapeType = ParticleSystemShapeType.Donut;
+            }
+            else if (effect.IsFlagSet(ParticleFlagBits.EmitterIsEllipsoid))
+            {
+                shape.shapeType = ParticleSystemShapeType.Sphere;
+            }
+            else
+            {
+                shape.shapeType = ParticleSystemShapeType.Box;
+            }
+        }
+
+        private static void SetParticleSpeedAndLifetime(ParticleSystem.MainModule main, ParticleEffect effect)
+        {
+            main.startLifetime = new ParticleSystem.MinMaxCurve(min: effect.Life.Life[0], max: effect.Life.Life[1]);
+            main.startSpeed =
+                new ParticleSystem.MinMaxCurve(min: effect.Emission.EmitSpeed[0], max: effect.Emission.EmitSpeed[1]);
+        }
+
+        private static void SetParticleDirection(ParticleSystem.VelocityOverLifetimeModule velocityOverTime,
+            ParticleEmission emission)
+        {
+            velocityOverTime.enabled = true;
+            velocityOverTime.x = new ParticleSystem.MinMaxCurve(emission.EmitDirection.LowerCorner[0],
+                emission.EmitDirection.UpperCorner[0]);
+            velocityOverTime.y = new ParticleSystem.MinMaxCurve(emission.EmitDirection.LowerCorner[1],
+                emission.EmitDirection.UpperCorner[1]);
+            velocityOverTime.z = new ParticleSystem.MinMaxCurve(emission.EmitDirection.LowerCorner[2],
+                emission.EmitDirection.UpperCorner[2]);
+        }
+
+        private static void SetParticleSizeOverTime(ParticleSystem.SizeOverLifetimeModule sizeOverTime, ParticleSize size)
+        {
+            sizeOverTime.size = size.SizeCurve.ToUnityCurveWithVariance(size.SizeVary);
+            sizeOverTime.enabled = true;
+        }
+
+        private static void SetParticleColorOverTime(ParticleSystem.ColorOverLifetimeModule colorOverLifetime,
+            ParticleColor color)
+        {
+            var (minColorGradient, maxColorGradient) = color.GetColorGradientsOverTime();
+            colorOverLifetime.color = new ParticleSystem.MinMaxGradient(minColorGradient, maxColorGradient);
+            colorOverLifetime.enabled = true;
         }
 
         private static Material MakeParticleMaterial(Texture texture, ParticleDrawing.DrawType drawType)
