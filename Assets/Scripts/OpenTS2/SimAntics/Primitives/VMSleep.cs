1b91222ff9882be587dbd37f3ebd71de2e7c9a54
Nahuel Rocchetti <lattechariot@gmail.com>
2023-08-22 12:49:02 -0300

    don't sleep a negative number

--- Assets/Scripts/OpenTS2/SimAntics/Primitives/VMSleep.cs
+++ Assets/Scripts/OpenTS2/SimAntics/Primitives/VMSleep.cs
@@ -14,8 +14,8 @@ namespace OpenTS2.SimAntics.Primitives
         public override VMReturnValue Execute(VMContext ctx)
         {
             var argumentIndex = ctx.Node.GetUInt16(0);
-            var sleepTicks = ctx.StackFrame.Arguments[argumentIndex];
-            return new VMReturnValue(new VMSleepContinueHandler(ctx.Stack, (uint)sleepTicks));
+            var sleepTicks = (uint)Math.Max(0,(int)ctx.StackFrame.Arguments[argumentIndex]);
+            return new VMReturnValue(new VMSleepContinueHandler(ctx.Stack, sleepTicks));
         }
     }
 }
