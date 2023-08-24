5b206ce74803da22545b8c8fb058ff00bb23d8e1
Nahuel Rocchetti <lattechariot@gmail.com>
2023-08-24 13:50:53 -0300

    Implement random number prim

---
+++ Assets/Scripts/OpenTS2/SimAntics/Primitives/VMRandomNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTS2.SimAntics.Primitives
+{
+    public class VMRandomNumber : VMPrimitive
+    {
+        public override VMReturnValue Execute(VMContext ctx)
+        {
+            var lhsSource = (VMDataSource)ctx.Node.GetOperand(2);
+            var lhsData = ctx.Node.GetInt16Operand(0);
+
+            var rhsSource = (VMDataSource)ctx.Node.GetOperand(6);
+            var rhsData = ctx.Node.GetInt16Operand(4);
+
+            var randomMaxValue = ctx.GetData(rhsSource, rhsData);
+            var randomFinalValue = (short)UnityEngine.Random.Range(0, randomMaxValue);
+
+            ctx.SetData(lhsSource, lhsData, randomFinalValue);
+
+            return VMReturnValue.ReturnTrue;
+        }
+    }
+}
