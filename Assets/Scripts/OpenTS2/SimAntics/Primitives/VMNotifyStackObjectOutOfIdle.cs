15e1beda476f166aaddd77d90f26da048576df73
Nahuel Rocchetti <lattechariot@gmail.com>
2023-08-24 15:36:41 -0300

    Update VMNotifyStackObjectOutOfIdle.cs

--- Assets/Scripts/OpenTS2/SimAntics/Primitives/VMNotifyStackObjectOutOfIdle.cs
+++ Assets/Scripts/OpenTS2/SimAntics/Primitives/VMNotifyStackObjectOutOfIdle.cs
@@ -10,10 +10,9 @@ namespace OpenTS2.SimAntics.Primitives
     {
         public override VMReturnValue Execute(VMContext ctx)
         {
-            var stackObject = ctx.VM.GetEntityByID(ctx.StackFrame.StackObjectID);
-            if (stackObject == null)
-                throw new SimAnticsException($"Couldn't find Object with ID {ctx.StackFrame.StackObjectID}", ctx.StackFrame);
-            ctx.VM.Scheduler.ScheduleInterrupt(ctx.StackObjectEntity.Stack);
+            var stackObj = ctx.StackObjectEntity;
+            if (stackObj != null)
+                ctx.VM.Scheduler.ScheduleInterrupt(stackObj.Stack);
             return VMReturnValue.ReturnTrue;
         }
     }
