4c4a365faf55b7fdcd9f46ed989217b3f5f1cb50
Nahuel Rocchetti <lattechariot@gmail.com>
2024-06-21 11:04:56 -0300

    Automatically ship shaders on build

---
+++ Assets/Editor/OpenTS2/BuildPreProcessor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BuildPreProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        var allshaders = Directory.GetFiles("Assets/Shaders", "*.shader", SearchOption.AllDirectories);
+        var graphicsSettings = AssetDatabase.LoadAssetAtPath<GraphicsSettings>("ProjectSettings/GraphicsSettings.asset");
+        var serializedObject = new SerializedObject(graphicsSettings);
+        var alwaysIncludedShadersProp = serializedObject.FindProperty("m_AlwaysIncludedShaders");
+
+        foreach(var shaderPath in allshaders)
+        {
+            var shader = AssetDatabase.LoadAssetAtPath<Shader>(shaderPath);
+            if (shader == null) continue;
+            for(var i=0;i<alwaysIncludedShadersProp.arraySize;i++)
+            {
+                var elem = alwaysIncludedShadersProp.GetArrayElementAtIndex(i);
+                if (elem.objectReferenceValue == shader)
+                    continue;
+            }
+            alwaysIncludedShadersProp.InsertArrayElementAtIndex(0);
+            var arrayElem = alwaysIncludedShadersProp.GetArrayElementAtIndex(0);
+            arrayElem.objectReferenceValue = shader;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
