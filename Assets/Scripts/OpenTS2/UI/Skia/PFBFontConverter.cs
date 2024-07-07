fd0c340af4a7472d8c8d35e50cad2e3b20f8377c
Nahuel Rocchetti <lattechariot@gmail.com>
2024-07-07 01:50:53 -0300

    use persistent data path for temp font files

--- Assets/Scripts/OpenTS2/UI/Skia/PFBFontConverter.cs
+++ Assets/Scripts/OpenTS2/UI/Skia/PFBFontConverter.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace OpenTS2.UI.Skia
 {
@@ -13,32 +14,37 @@ namespace OpenTS2.UI.Skia
     {
         private const string TXPath = "afdko/tx.exe";
         private const string MakeOTFPath = "afdko/makeotfexe.exe";
+        private static string TempPath = Path.Combine(Application.persistentDataPath, "Temp");
         public static bool ConvertFiles(string pfbPath, string otfPath)
         {
             var txPath = Path.Combine(Environment.CurrentDirectory, TXPath);
             var makeOtfPath = Path.Combine(Environment.CurrentDirectory, MakeOTFPath);
             if (!File.Exists(txPath) || !File.Exists(makeOtfPath))
                 return false;
-            var proc = Process.Start(txPath, $"-cff -Z +b \"{pfbPath}\" temp.font");
+            Directory.CreateDirectory(TempPath);
+            var temp1Path = Path.Combine(TempPath, "temp.font");
+            var temp2Path = Path.Combine(TempPath, "temp.font2");
+            var proc = Process.Start(txPath, $"-cff -Z +b \"{pfbPath}\" \"{temp1Path}\"");
             proc.WaitForExit();
-            proc = Process.Start(txPath, "-t1 temp.font temp.font2");
+            proc = Process.Start(txPath, $"-t1 \"{temp1Path}\" \"{temp2Path}\"");
             proc.WaitForExit();
-            proc = Process.Start(makeOtfPath, $"-f temp.font2 -o \"{otfPath}\"");
+            proc = Process.Start(makeOtfPath, $"-f \"{temp2Path}\" -o \"{otfPath}\"");
             proc.WaitForExit();
 
-            if (File.Exists("temp.font"))
-                File.Delete("temp.font");
-            if (File.Exists("temp.font2"))
-                File.Delete("temp.font2");
+            if (File.Exists(temp1Path))
+                File.Delete(temp1Path);
+            if (File.Exists(temp2Path))
+                File.Delete(temp2Path);
 
             return File.Exists(otfPath);
         }
 
         public static byte[] ConvertData(byte[] pfbData)
         {
+            Directory.CreateDirectory(TempPath);
             byte[] output = null;
-            var sourcePath = Path.Combine(Environment.CurrentDirectory, "afdko/temp_input.pfb");
-            var outputPath = Path.Combine(Environment.CurrentDirectory, "afdko/temp_output.otf");
+            var sourcePath = Path.Combine(TempPath, "temp_input.pfb");
+            var outputPath = Path.Combine(TempPath, "temp_output.otf");
 
             File.WriteAllBytes(sourcePath, pfbData);
 
